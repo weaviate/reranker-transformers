2fba5ba0f924b1e057ae17597ac8ce61b6bd4741
Marcin Antas <antas.marcin@gmail.com>
2023-06-14 11:28:47 +0200

    Add github actions

--- cicd/docker_push_custom_base.sh
+++ cicd/docker_push_custom_base.sh
@@ -2,24 +2,18 @@
 
 set -eou pipefail
 
-# Docker push rules
-# If on tag (e.g. 1.0.0)
-# - any commit is pushed as :<model>-<semver>
-# - any commit is pushed as :<model>-latest
-# - any commit is pushed as :<model>
 git_hash=
+pr=
 remote_repo=${REMOTE_REPO?Variable REMOTE_REPO is required}
 docker_username=${DOCKER_USERNAME?Variable DOCKER_USERNAME is required}
 docker_password=${DOCKER_PASSWORD?Variable DOCKER_PASSWORD is required}
-model_tag_name=${MODEL_TAG_NAME?Variable MODEL_TAG_NAME is required}
-model_name=${MODEL_NAME?Variable MODEL_NAME is required}
 git_tag=$GITHUB_REF_NAME
 
 function main() {
   init
-  echo "git ref type is $GITHUB_REF_TYPE"
-  echo "git ref name is $GITHUB_REF_NAME"
-  echo "git tag is $git_tag"
+  echo "git branch is $GIT_BRANCH"
+  echo "git tag is $GIT_TAG"
+  echo "pr is $pr"
   push_tag
 }
 
@@ -33,17 +27,18 @@ function init() {
 
 function push_tag() {
   if [ ! -z "$git_tag" ] && [ "$GITHUB_REF_TYPE" == "tag" ]; then
-    tag_git="$remote_repo:$model_tag_name-$git_tag"
-    tag_latest="$remote_repo:$model_tag_name-latest"
-    tag="$remote_repo:$model_tag_name"
+    tag_git="$remote_repo:custom-$git_tag"
+    tag_latest="$remote_repo:custom-latest"
+    tag="$remote_repo:custom"
 
     echo "Tag & Push $tag, $tag_latest, $tag_git"
-    docker buildx build --platform=linux/arm64,linux/amd64 \
-      --build-arg "MODEL_NAME=$model_name" \
-      --push \
-      --tag "$tag_git" \
-      --tag "$tag_latest" \
+    docker tag "custom-base" "$tag" && docker push "$tag"
+
+    docker buildx build -f custom.Dockerfile \
       --tag "$tag" \
+      --tag "$tag_latest" \
+      --tag "$tag_git" \
+      --push \
       .
   fi
 }
