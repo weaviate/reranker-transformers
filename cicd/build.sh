d77b38a834aa25e7b7403bd553f06721286d7b20
Marcin Antas <antas.marcin@gmail.com>
2023-06-14 11:05:44 +0200

    Update python dependencies, add smoke tests, adjust build.sh script

--- cicd/build.sh
+++ cicd/build.sh
@@ -4,7 +4,5 @@ set -e
 
 local_repo=${LOCAL_REPO?Variable LOCAL_REPO is required}
 model_name=${MODEL_NAME?Variable MODEL_NAME is required}
-onnx_runtime=${ONNX_RUNTIME:=false}
-onnx_cpu=${ONNX_CPU:=AVX512_VNNI}
 
-docker build --build-arg "MODEL_NAME=$model_name" --build-arg "ONNX_RUNTIME=$onnx_runtime" --build-arg "ONNX_CPU=$onnx_cpu" -t "$local_repo" .
+docker build --build-arg "MODEL_NAME=$model_name" -t "$local_repo" .
